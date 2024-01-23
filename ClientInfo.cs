93f67837e02d570498724689c36b3def283c1368
yodsaphonmonteerarat <122859465+yodsaphonmonteerarat@users.noreply.github.com>
2024-01-23 13:50:54 +0700

    Implemented ClientInfo.cs

--- ClientInfo.cs
+++ ClientInfo.cs
@@ -8,7 +8,6 @@ namespace DNWS
   {
     public ClientInfo()
     {
-    
     }
 
     public void PreProcessing(HTTPRequest request)
@@ -16,9 +15,22 @@ namespace DNWS
     }
     public HTTPResponse GetResponse(HTTPRequest request)
     {
+      string clientInfo = request.getPropertyByKey("RemoteEndPoint");
+      string[] clientInfoParts = clientInfo.Split(':');
+      string clientIP = clientInfoParts[0];
+      string clientPort = clientInfoParts[1];
+      string browserInfo = request.getPropertyByKey("User-Agent");
+      string acceptLanguage = request.getPropertyByKey("Accept-Language");
+      string acceptEncoding = request.getPropertyByKey("Accept-Encoding");
+
       HTTPResponse response = null;
       StringBuilder sb = new StringBuilder();
       sb.Append("<html><body><h1>ClientInfo:</h1>");
+      sb.Append("Client IP:" + clientIP + "<br>");
+      sb.Append("Client Port:" + clientPort + "<br>");
+      sb.Append("Browser Information: " + browserInfo + "<br>");
+      sb.Append("Accept Language: " + acceptLanguage + "<br>");
+      sb.Append("Accept Encoding: " + acceptEncoding + "<br>");
       sb.Append("</body></html>");
       response = new HTTPResponse(200);
       response.body = Encoding.UTF8.GetBytes(sb.ToString());
