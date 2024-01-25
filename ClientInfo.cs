6c3fb957b492d65a62a79f54ec8dc6dec241ee9b
TanatP1 <TanatP1@bitbucket.org>
2024-01-25 10:20:56 +0700

    Initial commit

---
+++ ClientInfo.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DNWS
+{
+  class ClientInfoPlugin : IPlugin
+  {
+    protected static Dictionary<String, int> statDictionary = null;
+    public ClientInfoPlugin()
+    {
+    }
+
+    public void PreProcessing(HTTPRequest request)
+    {
+      
+    }
+    public HTTPResponse GetResponse(HTTPRequest request)
+    {
+      HTTPResponse response = null;
+      StringBuilder sb = new StringBuilder();
+
+      string ClientInfo = request.getPropertyByKey("RemoteEndPoint");
+      string ClientIP = ClientInfo.Split(':')[0];
+      string ClientPort = ClientInfo.Split(':')[1];
+      string browserInfo = request.getPropertyByKey("User-Agent");
+      string acceptLanguage = request.getPropertyByKey("Accept-Language");
+      string acceptEncoding = request.getPropertyByKey("Accept-Encoding");
+
+      sb.Append("<html><head><style> body {font-family: courier new: 25px; } </style></head>");
+      sb.Append("<html><body><h1>ClientInfo:</h1>");
+      sb.Append("Client IP: " + ClientIP + "<br/>");
+      sb.Append("Client Port: " + ClientPort + "<br/>");
+      sb.Append("Client Browser Information: " + browserInfo + "<br/>");
+      sb.Append("Client Accept Language: " + acceptLanguage + "<br/>");
+      sb.Append("Client Accept Encoding: " + acceptEncoding + "<br/>");
+
+
+
+      sb.Append("</body></html>");
+      response = new HTTPResponse(200);
+      response.body = Encoding.UTF8.GetBytes(sb.ToString());
+      return response;
+    }
+
+    public HTTPResponse PostProcessing(HTTPResponse response)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
