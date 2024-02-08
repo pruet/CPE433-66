6245f3babed4f3e61942985c1f058bb084478785
Supatsala Charanaphong <supatsala_c@cmu.ac.th>
2024-02-08 22:40:19 +0700

    Final

--- Program.cs
+++ Program.cs
@@ -296,7 +296,22 @@ namespace DNWS
                     // Get one, show some info
                     _parent.Log("Client accepted:" + clientSocket.RemoteEndPoint.ToString());
                     HTTPProcessor hp = new HTTPProcessor(clientSocket, _parent);
-                    hp.Process();
+
+                    if(Program.Configuration["Threading"].ToLower() == "multi")
+                    {
+                       // Multi Thread
+                       TaskInfo ti = new TaskInfo(hp);
+                       ThreadPool.QueueUserWorkItem(new WaitCallback(ThreadProc), ti);
+                       
+   
+                    }
+                    else
+                    {
+                       //single Thread
+                        hp.Process();  
+
+                    }
+                    
                 }
                 catch (Exception ex)
                 {
