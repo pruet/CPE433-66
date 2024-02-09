9ef09c15849c0958731d2cb48c91e1b3895b386e
lab3 <natcha.aomsin@gmail.com>
2024-02-09 20:51:33 +0700

    final hw2

--- Program.cs
+++ Program.cs
@@ -296,7 +296,18 @@ namespace DNWS
                     // Get one, show some info
                     _parent.Log("Client accepted:" + clientSocket.RemoteEndPoint.ToString());
                     HTTPProcessor hp = new HTTPProcessor(clientSocket, _parent);
-                    hp.Process();
+
+                    if(Program.Configuration["Threading"].ToLower() == "multi")
+                    {
+                        //multi thread
+                        TaskInfo ti = nnew TaskInfo(hp);
+                        Threadpool.QueueUserWorkItem(new WaitCallBack(ThreadProc), ti);
+                    }
+                    else
+                    {
+                        //single thread
+                        hp.Process();
+                    }
                 }
                 catch (Exception ex)
                 {
