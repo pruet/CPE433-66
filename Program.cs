a3acf60df759b3610ab447f68e442f91f1da1f0d
yodsawin7 <yotsawin_thep@cmu.ac.th>
2024-02-09 15:02:43 +0700

    HW2 initial

--- Program.cs
+++ Program.cs
@@ -296,7 +296,25 @@ namespace DNWS
                     // Get one, show some info
                     _parent.Log("Client accepted:" + clientSocket.RemoteEndPoint.ToString());
                     HTTPProcessor hp = new HTTPProcessor(clientSocket, _parent);
-                    hp.Process();
+                   
+                    if(Program.Configuration["Threading"].ToLower() == "multi")
+                    {
+                        //Multi
+                        //TaskInfo ti = new TaskInfo(hp);
+                        //ThreadPool.QueueUserWorkItem(new WaitCallback(ThreadProc), ti);
+                        
+                        Thread thread = new Thread(new ParameterizedThreadStart(ThreadProc));
+                        thread.Start(new TaskInfo(hp));
+
+                    
+                    }else{
+                        //Single
+                        hp.Process();
+                     }
+
+
+
+                   
                 }
                 catch (Exception ex)
                 {
